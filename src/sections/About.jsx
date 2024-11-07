73e2dcb85390a048be81e4c1d7064fb37bb7d757
ritaban06 <ghoshritaban2006@gmail.com>
2024-11-07 17:50:30 +0530

    Changed my personal info

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -25,8 +25,8 @@ const About = () => {
             <div>
               <p className="grid-headtext">Hi, I'm Ritaban Ghosh</p>
               <p className="grid-subtext">
-                With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
-                and responsive websites.
+              I'm a 1st-year B.Tech IT college student passionate about coding, open-source, and creative projects. Skilled in React JS, 
+              Python, etc and eager to learn.
               </p>
             </div>
           </div>
@@ -58,12 +58,12 @@ const About = () => {
                 showGraticules
                 globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
                 bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                labelsData={[{ lat: 40, lng: -100, text: 'Rjieka, Croatia', color: 'white', size: 15 }]}
+                labelsData={[{ lat: 40, lng: -100, text: 'Kolkata, India', color: 'white', size: 15 }]}
               />
             </div>
             <div>
               <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-              <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
+              <p className="grid-subtext">I&apos;m based in Kolkata, India and open to remote work worldwide.</p>
               <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
             </div>
           </div>
