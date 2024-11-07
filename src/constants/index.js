ef39297891f70c6ae3ff6fd319797429d0743799
ritaban06 <ghoshritaban2006@gmail.com>
2024-11-07 23:48:45 +0530

    Add first 2 projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -58,12 +58,12 @@ export const navLinks = [
 
 export const myProjects = [
   {
-    title: 'Podcastr - AI Podcast Platform',
-    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+    title: 'Mind Map Creator',
+    desc: 'A React-based mind mapping tool that enables users to create, edit, and organize nodes in a visual, graph-like structure. Features include node creation, deletion, content editing, color customization, search, drag-and-drop, and canvas zooming/panning. Ideal for brainstorming and project planning, it offers a dynamic and intuitive visual organization experience.',
     subdesc:
-      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
-    texture: '/textures/project/project1.mp4',
+      '',
+    href: 'https://ritaban06.github.io/react-mind-map-creator/',
+    texture: '/textures/project/project6.mp4',
     logo: '/assets/one.svg',
     logoStyle: {
       backgroundColor: '#2A1816',
@@ -79,28 +79,18 @@ export const myProjects = [
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
-      },
-      {
-        id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
-      },
-      {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
+        name: 'Javascript',
+        path: 'assets/javascript.png',
       },
     ],
   },
   {
-    title: 'LiveDoc - Real-Time Google Docs Clone',
-    desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
+    title: 'Github Auto PR',
+    desc: 'AutoPRScheduler is a Python tool with a Tkinter GUI for automating GitHub pull requests. It allows users to schedule PRs to be submitted at a specified time, enhancing workflow automation. Features include automated PR creation, scheduling, and error handling. Requires Python 3, GitHub CLI, Tkinter, and tkcalendar for scheduling.',
     subdesc:
-      'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
-    href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
-    texture: '/textures/project/project2.mp4',
+      '',
+    href: 'https://github.com/ritaban06/github-auto-pr',
+    texture: '/textures/project/project6.mp4',
     logo: '/assets/two.svg',
     logoStyle: {
       backgroundColor: '#13202F',
@@ -111,23 +101,8 @@ export const myProjects = [
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
-      },
-      {
-        id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
-      },
-      {
-        id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
-      },
-      {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
+        name: 'Python',
+        path: '/assets/python.png',
       },
     ],
   },
@@ -137,7 +112,7 @@ export const myProjects = [
     subdesc:
       'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
     href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
-    texture: '/textures/project/project3.mp4',
+    texture: '/textures/project/project6.mp4',
     logo: '/assets/three.svg',
     logoStyle: {
       backgroundColor: '#60f5a1',
@@ -176,14 +151,14 @@ export const myProjects = [
     subdesc:
       'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
     href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
-    texture: '/textures/project/project4.mp4',
+    texture: '/textures/project/project6.mp4',
     logo: '/assets/four.svg',
     logoStyle: {
       backgroundColor: '#0E1F38',
       border: '0.2px solid #0E2D58',
       boxShadow: '0px 0px 60px 0px #2F67B64D',
     },
-    spotlight: '/assets/spotlight4.png',
+    spotlight: '/assets/spotlight6.png',
     tags: [
       {
         id: 1,
@@ -213,7 +188,7 @@ export const myProjects = [
     subdesc:
       'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
     href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
-    texture: '/textures/project/project5.mp4',
+    texture: '/textures/project/project6.mp4',
     logo: '/assets/five.svg',
     logoStyle: {
       backgroundColor: '#1C1A43',
