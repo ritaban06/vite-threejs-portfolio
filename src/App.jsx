dccd28f8b50e7c87604bda12078eb44716da58a9
ritaban06 <ghoshritaban2006@gmail.com>
2024-11-07 18:52:59 +0530

    Commented out projects part

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
-      <Projects />
+      {/* <Projects /> */}
       {/* <Clients /> */}
       {/* <WorkExperience /> */}
       <Contact />
