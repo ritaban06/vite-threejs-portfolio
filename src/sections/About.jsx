d73990b4ce4f524c3320805fb4a34299759ae948
ritaban06 <ghoshritaban2006@gmail.com>
2024-11-08 10:01:45 +0530

    Contact me btn works

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -64,7 +64,9 @@ const About = () => {
             <div>
               <p className="grid-headtext">I'm very flexible with time zone communications & locations</p>
               <p className="grid-subtext">I&apos;m based in Kolkata, India and open to remote work.</p>
+              <a href="#contact" className="w-fit">
               <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              </a>
             </div>
           </div>
         </div>
